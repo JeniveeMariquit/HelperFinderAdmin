b46736423e9d73362bc98ff96b3d34f2298a0bd7
joreggie <joannareggie@gmail.com>
2019-05-21 01:54:58 -0700

    redesigned

--- ratings.php
+++ ratings.php
@@ -2,47 +2,13 @@
     include 'header.php';
 ?>
     <body>
-        <div class="navbar navbar-fixed-top">
+    <div class="navbar navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">Edmin </a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.php">Edmin </a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
-                        <ul class="nav nav-icons">
-                            <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
-                            <li><a href="#"><i class="icon-eye-open"></i></a></li>
-                            <li><a href="#"><i class="icon-bar-chart"></i></a></li>
-                        </ul>
-                        <form class="navbar-search pull-left input-append" action="#">
-                        <input type="text" class="span3">
-                        <button class="btn" type="button">
-                            <i class="icon-search"></i>
-                        </button>
-                        </form>
-                        <ul class="nav pull-right">
-                            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-                                <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Item No. 1</a></li>
-                                    <li><a href="#">Don't Click</a></li>
-                                    <li class="divider"></li>
-                                    <li class="nav-header">Example Header</li>
-                                    <li><a href="#">A Separated link</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Support </a></li>
-                            <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="images/user.png" class="nav-avatar" />
-                                <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Your Profile</a></li>
-                                    <li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Logout</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+                        
                     </div>
                     <!-- /.nav-collapse -->
                 </div>
@@ -54,19 +20,16 @@
             <div class="container">
                 <div class="row">
                     <div class="span3">
-                       <div class="sidebar">
+                        <div class="sidebar">
                             <ul class="widget widget-menu unstyled">
-                                <li class="active"><a href="users.php"><i class="menu-icon icon-dashboard"></i>Users Record
+                                <li class="active"><a href="users.php"><i class="menu-icon icon-user"></i>Users
                                 </a></li>
-                                <li><a href="messages.php"><i class="menu-icon icon-bullhorn"></i>Message Record</a>
+                                <li><a href="messages.php"><i class="menu-icon icon-envelope"></i>Messages</a>
                                 </li>
-                                <li><a href="jobs.php"><i class="menu-icon icon-inbox"></i>Jobs <b class="label green pull-right">
-                                    11</b> </a></li>
-                                <li><a href="applicants.php"><i class="menu-icon icon-tasks"></i>Applicant <b class="label orange pull-right">
-                                    19</b> </a></li>
+                                <li><a href="jobs.php"><i class="menu-icon icon-tasks"></i>Jobs</a></li>
+                                <li><a href="applicants.php"><i class="menu-icon icon-group"></i>Applicants </a></li>
 
-                                    <li><a href="ratings.php"><i class="menu-icon icon-tasks"></i>Ratings <b class="label orange pull-right">
-                                    19</b> </a></li>
+                                    <li><a href="ratings.php"><i class="menu-icon icon-star"></i>Ratings </a></li>
                             </ul>
                             <!--/.widget-nav-->
                             
@@ -102,7 +65,7 @@
                             <div class="module">
                                 <div class="module-head">
                                     <h3 id="test">
-                                        Rating Record</h3>
+                                        Ratings Record</h3>
                                 </div>
                                 <div class="module-body table">
                                     <table id="emp_table" cellpadding="0" cellspacing="0" border="0" class=" table table-bordered table-striped"
